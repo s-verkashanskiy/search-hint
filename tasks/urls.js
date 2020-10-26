63abaf5cc6aa5d7bbb5903842a68edfe75b18e63
sergey <stype@yandex.ru>
2020-10-26 16:41:14 +0300

    26-10-2020

---
+++ tasks/urls.js
@@ -0,0 +1,5 @@
+// https://yandex.ru/search/?lr=213&text=elasticsearch%20ngram%20%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B
+// https://techarks.ru/qa/polzovatelskij-analizator-KJ/
+// https://devacademy.ru/article/effektivnaya-nastrojka-elasticsearch/
+// 
+// 
